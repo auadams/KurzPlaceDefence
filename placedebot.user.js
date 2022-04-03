ed59072467a4e71d474f47bad999d489dc7519b1
Austin Adams <33039166+auadams@users.noreply.github.com>
2022-04-03 18:36:57 -0400

    Update placedebot.user.js

--- placedebot.user.js
+++ placedebot.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Kurzgesagt_defence
 // @namespace    https://github.com/PlaceNL/Bot
-// @version      1
+// @version      2
 // @description  Forked from PlaceNL!
 // @author       Austin
 // @match        https://www.reddit.com/r/place/*
