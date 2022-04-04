b60fe250fa8970a90de3266081feccc9968eb4f3
Austin Adams <33039166+auadams@users.noreply.github.com>
2022-04-04 07:11:12 -0400

    thing

--- placedebot.user.js
+++ placedebot.user.js
@@ -1,356 +1,291 @@
 // ==UserScript==
-// @name         Kurzgesagt_defence
-// @namespace    https://github.com/PlaceNL/Bot
-// @version      5
-// @description  Forked from PlaceNL!
-// @author       Austin
+// @name         KurrzDefence
+// @namespace    https://github.com/auadams/KurzPlaceDefence
+// @version      9
+// @description  /r/kurzgesagt_place
+// @author       austin
 // @match        https://www.reddit.com/r/place/*
 // @match        https://new.reddit.com/r/place/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
-// @require      https://cdn.jsdelivr.net/npm/toastify-js
+// @require	     https://cdn.jsdelivr.net/npm/toastify-js
 // @resource     TOASTIFY_CSS https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css
-// @grant        GM_getResourceText
-// @grant        GM_addStyle
 // @updateURL    https://github.com/auadams/KurzPlaceDefence/raw/main/placedebot.user.js
 // @downloadURL  https://github.com/auadams/KurzPlaceDefence/raw/main/placedebot.user.js
+// @grant        GM_getResourceText
+// @grant        GM_addStyle
 // ==/UserScript==
 
+// Ignore that hideous code. But if it works, it works.
 
+const VERSION = 9;
 
-var socket;
-var order = undefined;
-var accessToken;
-var currentOrderCanvas = document.createElement('canvas');
-var currentOrderCtx = currentOrderCanvas.getContext('2d');
-var currentPlaceCanvas = document.createElement('canvas');
+const PLACE_URL = 'https://gql-realtime-2.reddit.com/query';
+const UPDATE_URL = 'https://github.com/auadams/KurzPlaceDefence/raw/main/placedebot.user.js';
 
-const COLOR_MAPPINGS = {
-	'#6D001A': 0,
-	'#BE0039': 1,
-	'#FF4500': 2,
-	'#FFA800': 3,
-	'#FFD635': 4,
-	'#FFF8B8': 5,
-	'#00A368': 6,
-	'#00CC78': 7,
-	'#7EED56': 8,
-	'#00756F': 9,
-	'#009EAA': 10,
-	'#00CCC0': 11,
-	'#2450A4': 12,
-	'#3690EA': 13,
-	'#51E9F4': 14,
-	'#493AC1': 15,
-	'#6A5CFF': 16,
-	'#94B3FF': 17,
-	'#811E9F': 18,
-	'#B44AC0': 19,
-	'#E4ABFF': 20,
-	'#DE107F': 21,
-	'#FF3881': 22,
-	'#FF99AA': 23,
-	'#6D482F': 24,
-	'#9C6926': 25,
-	'#FFB470': 26,
-	'#000000': 27,
-	'#515252': 28,
-	'#898D90': 29,
-	'#D4D7D9': 30,
-	'#FFFFFF': 31
-};
+let accessToken;
+let canvas = document.createElement('canvas');
 
-let getRealWork = rgbaOrder => {
-    let order = [];
-    for (var i = 0; i < 2000000; i++) {
-        if (rgbaOrder[(i * 4) + 3] !== 0) {
-            order.push(i);
-        }
-    }
-    return order;
-};
-
-let getPendingWork = (work, rgbaOrder, rgbaCanvas) => {
-    let pendingWork = [];
-    for (const i of work) {
-        if (rgbaOrderToHex(i, rgbaOrder) !== rgbaOrderToHex(i, rgbaCanvas)) {
-            pendingWork.push(i);
-        }
-    }
-    return pendingWork;
-};
+let ccConnection;
+let timeout;
+let onCooldown;
 
 (async function () {
-    GM_addStyle(GM_getResourceText('TOASTIFY_CSS'));
-    currentOrderCanvas.width = 2000;
-    currentOrderCanvas.height = 2000;
-    currentOrderCanvas.style.display = 'none';
-    currentOrderCanvas = document.body.appendChild(currentOrderCanvas);
-    currentPlaceCanvas.width = 2000;
-    currentPlaceCanvas.height = 2000;
-    currentPlaceCanvas.style.display = 'none';
-    currentPlaceCanvas = document.body.appendChild(currentPlaceCanvas);
+	GM_addStyle(GM_getResourceText('TOASTIFY_CSS'));
 
-    Toastify({
-        text: 'getting token',
-        duration: 10000
-    }).showToast();
-    accessToken = await getAccessToken();
-    Toastify({
-        text: 'got token!',
-        duration: 10000
-    }).showToast();
+	canvas.width = 2000;
+	canvas.height = 2000;
+	canvas = document.body.appendChild(canvas);
 
-    connectSocket();
-    attemptPlace();
+	await new Promise(r => setTimeout(r, 1000));
 
-    setInterval(() => {
-        if (socket) socket.send(JSON.stringify({ type: 'ping' }));
-    }, 5000);
+	await initToken();
+	initServerConnection();
 })();
 
-function connectSocket() {
-    Toastify({
-        text: 'Connecting to r/kurzgesagt defence...',
-        duration: 10000
-    }).showToast();
-
-    socket = new WebSocket('wss://topsneaky.birdbot.xyz/api/ws');
-
-    socket.onopen = function () {
-        Toastify({
-            text: 'Connected Kurzgesagt defence server!',
-            duration: 10000
-        }).showToast();
-        socket.send(JSON.stringify({ type: 'getmap' }));
-    };
-
-    socket.onmessage = async function (message) {
-        var data;
-        try {
-            data = JSON.parse(message.data);
-        } catch (e) {
-            return;
-        }
-
-        switch (data.type.toLowerCase()) {
-            case 'map':
-                Toastify({
-                    text: `Collecting data (reden: ${data.reason ? data.reason : 'from server'})...`,
-                    duration: 10000
-                }).showToast();
-                currentOrderCtx = await getCanvasFromUrl(`https://topsneaky.birdbot.xyz/maps/${data.data}`, currentOrderCanvas);
-                order = getRealWork(currentOrderCtx.getImageData(0, 0, 2000, 2000).data);
-                Toastify({
-                    text: `Artwork has, ${order.length} pixels in total`,
-                    duration: 10000
-                }).showToast();
-                break;
-            default:
-                break;
-        }
-    };
-
-    socket.onclose = function (e) {
-        Toastify({
-            text: `Disconnected from KGS defence server: ${e.reason}`,
-            duration: 10000
-        }).showToast();
-        console.error('Error: ', e.reason);
-        socket.close();
-        setTimeout(connectSocket, 10000);
-    };
+async function initToken() {
+	// Create AccessToken
+	Toastify({
+		text: 'Frage Zugriffstokens an...',
+		duration: 10000,
+		gravity: "bottom",
+		style: {
+			background: '#C6C6C6',
+			color: '#111'
+		},
+	}).showToast();
+	accessToken = await getAccessToken();
+	Toastify({
+		text: 'Zugriffstoken erhalten!',
+		duration: 10000,
+		gravity: "bottom",
+		style: {
+			background: '#92E234',
+		},
+	}).showToast();
 }
 
-async function attemptPlace() {
-    if (order === undefined) {
-        setTimeout(attemptPlace, 2000); // probeer opnieuw in 2sec.
-        return;
-    }
-    var ctx;
-    try {
-        ctx = await getCanvasFromUrl(await getCurrentImageUrl('0'), currentPlaceCanvas, 0, 2000);
-    } catch (e) {
-        console.warn('Placing square on map: ', e);
-        Toastify({
-            text: 'Placement will try in 10 sec...',
-            duration: 10000
-        }).showToast();
-        setTimeout(attemptPlace, 10000); // probeer opnieuw in 10sec.
-        return;
-    }
+async function initServerConnection() {
+	// Establish connection to command&control server
+	Toastify({
+		text: 'Verbinde mit dem Kommando-Server...',
+		duration: 10000,
+		gravity: "bottom",
+		style: {
+			background: '#C6C6C6',
+			color: '#111'
+		},
+	}).showToast();
 
-    const rgbaOrder = currentOrderCtx.getImageData(0, 0, 2000, 2000).data;
-    const rgbaCanvas = ctx.getImageData(0, 0, 2000, 2000).data;
-    const work = getPendingWork(order, rgbaOrder, rgbaCanvas);
+	ccConnection = new WebSocket('wsS://topsneaky.birdbot.xyz');
+	ccConnection.onopen = function () {
+		Toastify({
+			text: 'Verbindung zum Server aufgebaut!',
+			duration: 10000,
+			gravity: "bottom",
+			style: {
+				background: '#92E234',
+			},
+		}).showToast();
 
-    if (work.length === 0) {
-        Toastify({
-            text: `All pixels are already in the right place! Try again in 30 sec...`,
-            duration: 30000
-        }).showToast();
-        setTimeout(attemptPlace, 30000); // probeer opnieuw in 30sec.
-        return;
-    }
+		// handshake
+		ccConnection.send(JSON.stringify({"operation":"handshake","data":{"platform":"browser","version":VERSION,"useraccounts":1}}));
+		setReady()
+	}
+	ccConnection.onerror = function (error) {
+		Toastify({
+			text: 'Verbindung zum Server fehlgeschlagen!',
+			duration: 10000,
+			gravity: "bottom",
+			style: {
+				background: '#ED001C',
+			},
+		}).showToast();
+		console.log('WebSocket Error: '+ error);
+	};
+	ccConnection.onclose = function (close) {
+		Toastify({
+			text: 'Verbindung zum Server unterbrochen! Verbinde neu in 10 Sekunden...',
+			duration: 10000,
+			gravity: "bottom",
+			style: {
+				background: '#ED001C',
+			},
+		}).showToast();
+		console.log('WebSocket Close: '+ close.code);
+		if (close.code === 1006) {
+			Toastify({
+				text: 'Mögliches Problem mit deinem Adblocker etc.',
+				duration: 30000,
+				gravity: "bottom",
+				style: {
+					background: '#ED001C',
+				},
+			}).showToast();
+		}
 
-    const percentComplete = 100 - Math.ceil(work.length * 100 / order.length);
-    const idx = Math.floor(Math.random() * work.length);
-    const i = work[idx];
-    const x = i % 2000;
-    const y = Math.floor(i / 2000);
-    const hex = rgbaOrderToHex(i, rgbaOrder);
-
-    Toastify({
-        text: `Trying to post pixel on ${x}, ${y}... (${percentComplete}% complete)`,
-        duration: 10000
-    }).showToast();
-
-    const res = await place(x, y, COLOR_MAPPINGS[hex]);
-    const data = await res.json();
-    try {
-        if (data.errors) {
-            const error = data.errors[0];
-            const nextPixel = error.extensions.nextAvailablePixelTs + 3000;
-            const nextPixelDate = new Date(nextPixel);
-            const delay = nextPixelDate.getTime() - Date.now();
-            Toastify({
-                text: `Pixel posted too soon! Next pixel will be placed at ${nextPixelDate.toLocaleTimeString()}.`,
-                duration: delay
-            }).showToast();
-            setTimeout(attemptPlace, 10000);
-        } else {
-            const nextPixel = data.data.act.data[0].data.nextAvailablePixelTimestamp + 3000;
-            const nextPixelDate = new Date(nextPixel);
-            const delay = nextPixelDate.getTime() - Date.now();
-            Toastify({
-                text: `Pixel posted on ${x}, ${y}! Next pixel will be placed at ${nextPixelDate.toLocaleTimeString()}.`,
-                duration: delay
-            }).showToast();
-            setTimeout(attemptPlace, delay);
-        }
-    } catch (e) {
-        console.warn('Analyze response error', e);
-        Toastify({
-            text: `Analyze response error: ${e}.`,
-            duration: 10000
-        }).showToast();
-        setTimeout(attemptPlace, 10000);
-    }
+		setTimeout(() => initServerConnection(), 10*1000);
+	};
+	ccConnection.onmessage  = processOperation;
 }
 
-function place(x, y, color) {
-    socket.send(JSON.stringify({ type: 'placepixel', x, y, color }));
-    return fetch('https://gql-realtime-2.reddit.com/query', {
-        method: 'POST',
-        body: JSON.stringify({
-            'operationName': 'setPixel',
-            'variables': {
-                'input': {
-                    'actionName': 'r/replace:set_pixel',
-                    'PixelMessageData': {
-                        'coordinate': {
-                            'x': x % 1000,
-                            'y': y % 1000
-                        },
-                        'colorIndex': color,
-                        'canvasIndex': (x > 999 ? 1 : 0)
-                    }
-                }
-            },
-            'query': 'mutation setPixel($input: ActInput!) {\n  act(input: $input) {\n    data {\n      ... on BasicMessage {\n        id\n        data {\n          ... on GetUserCooldownResponseMessageData {\n            nextAvailablePixelTimestamp\n            __typename\n          }\n          ... on SetPixelResponseMessageData {\n            timestamp\n            __typename\n          }\n          __typename\n        }\n        __typename\n      }\n      __typename\n    }\n    __typename\n  }\n}\n'
-        }),
-        headers: {
-            'origin': 'https://hot-potato.reddit.com',
-            'referer': 'https://hot-potato.reddit.com/',
-            'apollographql-client-name': 'mona-lisa',
-            'Authorization': `Bearer ${accessToken}`,
-            'Content-Type': 'application/json'
-        }
-    });
+function processOperation(message) {
+	// console.log('WebSocket Message received: '+message.data);
+	const messageData = JSON.parse(message.data);
+	switch (messageData.operation) {
+		case 'place-pixel':
+			void processOperationPlacePixel(messageData.data);
+			return;
+		case 'notify-update':
+			void processOperationNotifyUpdate(messageData.data);
+			return;
+	}
+}
+
+async function processOperationPlacePixel(data) {
+	onCooldown = true;
+
+	const x = data.x;
+	const y = data.y;
+	const color = data.color;
+
+	const time = new Date().getTime();
+	let nextAvailablePixelTimestamp = await place(x, y, color) ?? new Date(time + 1000 * 60 * 5 + 1000 * 15)
+
+	// Sanity check timestamp
+	if (nextAvailablePixelTimestamp < time || nextAvailablePixelTimestamp > time + 1000 * 60 * 5 + 1000 * 15) {
+		nextAvailablePixelTimestamp = time + 1000 * 60 * 5 + 1000 * 15;
+	}
+
+	// Add a few random seconds to the next available pixel timestamp
+	const waitFor = nextAvailablePixelTimestamp - time + (Math.random() * 1000 * 15);
+
+	const minutes = Math.floor(waitFor / (1000 * 60))
+	const seconds = Math.floor((waitFor / 1000) % 60)
+	Toastify({
+		text: `Noch ${minutes}m ${seconds}s Abklingzeit bis ${new Date(nextAvailablePixelTimestamp).toLocaleTimeString()} Uhr`,
+		duration: waitFor,
+		gravity: "bottom",
+		style: {
+			background: '#FF5700',
+		},
+	}).showToast();
+	timeout = setTimeout(setReady, waitFor);
+}
+
+async function processOperationNotifyUpdate(data) {
+	Toastify({
+		text: `Neue Script-Version verfügbar! Aktulaisiere unter ${UPDATE_URL}`,
+		duration: 10000,
+		gravity: "bottom",
+		style: {
+			background: '#ED001C',
+		},
+	}).showToast();
+}
+
+function setReady() {
+	clearTimeout(timeout);
+	onCooldown = false;
+	ccConnection.send(JSON.stringify({"operation":"request-pixel","user":"browser-script"}));
+	setTimeout(() => checkForIdle(), 60*1000);
+}
+
+function checkForIdle() {
+	// send new request if server didn't answer with job
+	if (!onCooldown) {
+		setReady();
+	}
+}
+
+function getCanvasId(x,y) {
+	return (x > 1000) + (y > 1000) * 2;
+}
+/**
+ * Places a pixel on the canvas, returns the "nextAvailablePixelTimestamp", if succesfull
+ * @param x
+ * @param y
+ * @param color
+ * @returns {Promise<number>}
+ */
+async function place(x, y, color) {
+	const response = await fetch(PLACE_URL, {
+		method: 'POST',
+		body: JSON.stringify({
+			'operationName': 'setPixel',
+			'variables': {
+				'input': {
+					'actionName': 'r/replace:set_pixel',
+					'PixelMessageData': {
+						'coordinate': {
+							'x': x % 1000,
+							'y': y % 1000
+						},
+						'colorIndex': color,
+						'canvasIndex': getCanvasId(x,y)
+					}
+				}
+			},
+			'query': `mutation setPixel($input: ActInput!) {
+				act(input: $input) {
+					data {
+						... on BasicMessage {
+							id
+							data {
+								... on GetUserCooldownResponseMessageData {
+									nextAvailablePixelTimestamp
+									__typename
+								}
+								... on SetPixelResponseMessageData {
+									timestamp
+									__typename
+								}
+								__typename
+							}
+							__typename
+						}
+						__typename
+					}
+					__typename
+				}
+			}
+			`
+		}),
+		headers: {
+			'origin': 'https://hot-potato.reddit.com',
+			'referer': 'https://hot-potato.reddit.com/',
+			'apollographql-client-name': 'mona-lisa',
+			'Authorization': `Bearer ${accessToken}`,
+			'Content-Type': 'application/json'
+		}
+	});
+	const data = await response.json()
+	if (data.errors !== undefined) {
+		Toastify({
+			text: 'Pixel konnte nicht plaziert werden, da du noch Abklingzeit hast...',
+			duration: 10000,
+			gravity: "bottom",
+			style: {
+				background: '#ED001C',
+			},
+		}).showToast();
+		return data.errors[0].extensions?.nextAvailablePixelTs
+	}
+	Toastify({
+		text: `Pixel gesetzt auf x:${x} y:${y}`,
+		duration: 10000,
+		gravity: "bottom",
+		style: {
+			background: '#92E234',
+		},
+	}).showToast();
+	return data?.data?.act?.data?.[0]?.data?.nextAvailablePixelTimestamp
 }
 
 async function getAccessToken() {
-    const usingOldReddit = window.location.href.includes('new.reddit.com');
-    const url = usingOldReddit ? 'https://new.reddit.com/r/place/' : 'https://www.reddit.com/r/place/';
-    const response = await fetch(url);
-    const responseText = await response.text();
+	const usingOldReddit = window.location.href.includes('new.reddit.com');
+	const url = usingOldReddit ? 'https://new.reddit.com/r/place/' : 'https://www.reddit.com/r/place/';
+	const response = await fetch(url);
+	const responseText = await response.text();
 
-    // TODO: ew
-    return responseText.split('\"accessToken\":\"')[1].split('"')[0];
+	return responseText.match(/"accessToken"\s*:\s*"([\w-]+)"/)[1];
 }
-
-async function getCurrentImageUrl(id = '0') {
-    return new Promise((resolve, reject) => {
-        const ws = new WebSocket('wss://gql-realtime-2.reddit.com/query', 'graphql-ws');
-
-        ws.onopen = () => {
-            ws.send(JSON.stringify({
-                'type': 'connection_init',
-                'payload': {
-                    'Authorization': `Bearer ${accessToken}`
-                }
-            }));
-            ws.send(JSON.stringify({
-                'id': '1',
-                'type': 'start',
-                'payload': {
-                    'variables': {
-                        'input': {
-                            'channel': {
-                                'teamOwner': 'AFD2022',
-                                'category': 'CANVAS',
-                                'tag': id
-                            }
-                        }
-                    },
-                    'extensions': {},
-                    'operationName': 'replace',
-                    'query': 'subscription replace($input: SubscribeInput!) {\n  subscribe(input: $input) {\n    id\n    ... on BasicMessage {\n      data {\n        __typename\n        ... on FullFrameMessageData {\n          __typename\n          name\n          timestamp\n        }\n      }\n      __typename\n    }\n    __typename\n  }\n}'
-                }
-            }));
-        };
-
-        ws.onmessage = (message) => {
-            const { data } = message;
-            const parsed = JSON.parse(data);
-
-            // TODO: ew
-            if (!parsed.payload || !parsed.payload.data || !parsed.payload.data.subscribe || !parsed.payload.data.subscribe.data) return;
-
-            ws.close();
-            resolve(parsed.payload.data.subscribe.data.name + `?noCache=${Date.now() * Math.random()}`);
-        }
-
-        ws.onerror = reject;
-    });
-}
-
-function getCanvasFromUrl(url, canvas, x = 0, y = 0) {
-    return new Promise((resolve, reject) => {
-        let loadImage = ctx => {
-            var img = new Image();
-            img.crossOrigin = "no-referrer-when-downgrade";
-            img.origin="*";
-            img.onload = () => {
-                ctx.drawImage(img, x, y);
-                resolve(ctx);
-            };
-            img.onerror = () => {
-                Toastify({
-                    text: 'Error retrieving orders. Try again in 10 sec...',
-                    duration: 5000
-                }).showToast();
-                setTimeout(() => loadImage(ctx), 10000);
-                return;
-            };
-            img.src = url;
-        };
-        loadImage(canvas.getContext('2d'));
-    });
-}
-
-function rgbToHex(r, g, b) {
-    return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1).toUpperCase();
-}
-
-let rgbaOrderToHex = (i, rgbaOrder) =>
-    rgbToHex(rgbaOrder[i * 4], rgbaOrder[i * 4 + 1], rgbaOrder[i * 4 + 2]);
